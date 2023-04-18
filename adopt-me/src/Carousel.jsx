cab0e7d2be19a3fd70c15c49d2c063558c56e7b3
dahighsky <aakashuttkarsh29@gmail.com>
2023-04-18 17:49:18 +0530

    sth

--- adopt-me/src/Carousel.jsx
+++ adopt-me/src/Carousel.jsx
@@ -8,6 +8,18 @@ class Carousel extends Component {
   static defaultProps = {
     images: ["http://pets-images.dev-apis.com/pets/none.jpg"],
   };
+
+  componentDidMount() {
+    console.log("Details Page");
+  }
+  componentWillUnmount() {
+    console.log("Details Page Exited");
+  }
+  handleCilck = (e) => {
+    this.setState({
+      active: e.target.dataset.index,
+    });
+  };
   render() {
     const { active } = this.state;
     const { images } = this.props;
@@ -17,11 +29,14 @@ class Carousel extends Component {
         <img src={images[active]} alt="main active" />
         <div className="carousel-smaller">
           {images.map((image, index) => (
+            // eslint-disable-next-line
             <img
               key={image}
               src={image}
+              data-index={index}
               className={index === active ? "active" : ""}
               alt="animal thumbnail"
+              onClick={this.handleCilck}
             />
           ))}
         </div>
